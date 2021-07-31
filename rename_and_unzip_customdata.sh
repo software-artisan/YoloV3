85488ba2c82c6be5e734671c64f492d2bab8dae5
Raja Rajendran <raj@infinstor.com>
2021-08-01 00:07:30 +0530

    added script to rename data/customdata and to unzip customdata.zip

---
+++ rename_and_unzip_customdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -z "$2" ] && { echo "Usage: $0 <zip_file_name> <yolov3_dir_path>"; exit 1; } 
+
+set -xq
+# if data/customdata_walle does not exist, 
+if [ ! -d "$2/data/customdata_walle" ]; then
+    # then rename data/customdata to data/customdata_walle
+    mv -v "$2/data/customdata" "$2/data/customdata_walle"
+    
+    # unzip customdata.zip.  this creates data/customdata directory
+    cd $2/data
+    unzip $1 
+fi;
